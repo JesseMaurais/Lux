3faa191cba8de1cb63c02e3f2d70a94ff7bbe367
Jesse Maurais <jessemaurais@gmail.com>
2015-07-24 08:11:57 -0600

    Added float.h constants to fenv module

--- fenv.cpp
+++ fenv.cpp
@@ -1,4 +1,5 @@
 #include "lux.hpp"
+#include <cfloat>
 #include <cfenv>
 
 extern "C" int luaopen_fenv(lua_State *state)
@@ -7,16 +8,16 @@ extern "C" int luaopen_fenv(lua_State *state)
 	luaL_Reg regs[] =
 	{
 	{"clear", lux_cast(feclearexcept)},
-	{"getflag", lux_cast(fegetexceptflag)},
 	{"raise", lux_cast(feraiseexcept)},
-	{"setflag", lux_cast(fesetexceptflag)},
 	{"test", lux_cast(fetestexcept)},
+	{"hold", lux_cast(feholdexcept)},
+	{"update", lux_cast(feupdateenv)},
+	{"getflag", lux_cast(fegetexceptflag)},
+	{"setflag", lux_cast(fesetexceptflag)},
 	{"getround", lux_cast(fegetround)},
 	{"setround", lux_cast(fesetround)},
 	{"getenv", lux_cast(fegetenv)},
-	{"hold", lux_cast(feholdexcept)},
 	{"setenv", lux_cast(fesetenv)},
-	{"update", lux_cast(feupdateenv)},
 	{nullptr}
 	};
 	luaL_setfuncs(state, regs, 0);
@@ -36,6 +37,29 @@ extern "C" int luaopen_fenv(lua_State *state)
 	{"TONEAREST", FE_TONEAREST},
 	{"TOWARDZERO", FE_TOWARDZERO},
 	{"UPWARD", FE_UPWARD},
+	// cfloat
+	{"RADIX", FLT_RADIX},
+	{"ROUNDS", FLT_ROUNDS},
+	{"EVAL_METHOD", FLT_EVAL_METHOD},
+	{"DECIMAL_DIG", DECIMAL_DIG},
+	{"FLT_MANT_DIG", FLT_MANT_DIG},
+	{"DBL_MANT_DIG", FLT_MANT_DIG},
+	{"LDBL_MANT_DIG", LDBL_MANT_DIG},
+	{"FLT_DIG", FLT_DIG},
+	{"DBL_DIG", DBL_DIG},
+	{"LDBL_DIG", LDBL_DIG},
+	{"FLT_MIN_EXP", FLT_MIN_EXP},
+	{"DBL_MIN_EXP", DBL_MIN_EXP},
+	{"LDBL_MIN_EXP", LDBL_MIN_EXP},
+	{"FLT_MIN_1O_EXP", FLT_MIN_10_EXP},
+	{"DBL_MIN_10_EXP", DBL_MIN_10_EXP},
+	{"LDBL_MIN_10_EXP", LDBL_MIN_10_EXP},
+	{"FLT_MAX_EXP", FLT_MAX_EXP},
+	{"DBL_MAX_EXP", DBL_MAX_EXP},
+	{"LDBL_MAX_EXP", LDBL_MAX_EXP},
+	{"FLT_MAX_10_EXP", FLT_MAX_10_EXP},
+	{"DBL_MAX_10_EXP", DBL_MAX_10_EXP},
+	{"LDBL_MAX_10_EXP", LDBL_MAX_10_EXP},
 	{nullptr}
 	};
 	for (auto r=args; r->name; ++r)
@@ -43,8 +67,12 @@ extern "C" int luaopen_fenv(lua_State *state)
 	 lua_pushinteger(state, r->value);
 	 lua_setfield(state, -2, r->name);
 	}
-	lux_push(state, FE_DFL_ENV);
-	lua_setfield(state, -2, "default");
+	lux_push(state, FLT_EPSILON);
+	lua_setfield(state, -2, "FLT_EPSILON");
+	lux_push(state, DBL_EPSILON);
+	lua_setfield(state, -2, "DBL_EPSILON");
+	lux_push(state, LDBL_EPSILON);
+	lua_setfield(state, -2, "LDBL_EPSILON");
 	return 1;
 }
 
